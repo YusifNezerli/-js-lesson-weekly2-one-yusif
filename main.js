b6f6a70288ba4b9dee87e322a190f35ef79e0b0b
YusifNezerli <nezerliyusif42@mail.ru>
2023-01-08 20:15:11 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+alert("restonanimiza xos gelmisiniz")
+alert("yemek secimi")
+let a=prompt("toyuq yoxsa et")
+
+let b=prompt("corek yoxsa lavas")
+
+let c=prompt("boyuk yoxsa balaca ")
+
+let d=prompt("ayran yoxsa cola")
+
+let e=prompt("nece neferlik")
+
+let f=prompt("boyuk zal ve ya otaq")
+
+
+alert("sizin sifarisiniz" +","+ a + ","  + b +"," + c + "," + d  )
+
+
